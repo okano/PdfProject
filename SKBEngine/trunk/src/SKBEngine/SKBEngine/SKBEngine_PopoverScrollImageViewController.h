24af676ce4723f19836e6bef8c5154fb3752205e
kounago <kounago@8b35791b-02c8-de11-b75f-001d920307b8>
2011-04-23 08:24:16 +0000

    add SKBEngine_PopoverScrollImageViewController.
    git-svn-id: svn+ssh://kounago.sakura.ne.jp/home/kounago/svn-repos/test1/iPad/PdfProject@2827 8b35791b-02c8-de11-b75f-001d920307b8

---
+++ SKBEngine/trunk/src/SKBEngine/SKBEngine/SKBEngine_PopoverScrollImageViewController.h
@@ -0,0 +1,16 @@
+//
+//  SKBEngine_PopoverScrollImageViewController.h
+//  SKBEngine
+//
+//  Created by okano on 11/04/23.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SKBEngine_PopoverScrollImageViewController : UIViewController {
+    
+}
+
+@end
