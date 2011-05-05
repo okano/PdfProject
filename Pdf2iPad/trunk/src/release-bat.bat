46ecd07325541191dc814f86223b21c3b292083b
okano <okano@synergysoft.jp>
2011-05-05 23:10:35 +0900

    change folder name in release-bat.bat

--- Pdf2iPad/trunk/src/release-bat.bat
+++ Pdf2iPad/trunk/src/release-bat.bat
@@ -1,15 +1,15 @@
-del Pdf2iPad.zip
+del JPPBook.zip
 rmdir /S /Q release-wrk
 mkdir release-wrk
-mkdir release-wrk\Pdf2iPad
-xcopy /S /E /I /Q Pdf2iPad release-wrk\Pdf2iPad
-mkdir release-wrk\Pdf2iPad\SKBEngine
-xcopy /S /E /I /Q ..\..\..\SKBEngine\trunk\src\SKBEngine\SKBEngine\SKB*.? release-wrk\Pdf2iPad\SKBEngine
+mkdir release-wrk\JPPBook
+xcopy /S /E /I /Q JPPBook release-wrk\JPPBook
+mkdir release-wrk\JPPBook\SKBEngine
+xcopy /S /E /I /Q ..\..\..\SKBEngine\trunk\src\SKBEngine\SKBEngine\SKB*.? release-wrk\JPPBook\SKBEngine
 
 cd release-wrk
-"C:\Program Files\à≥èkÅEìWäJ\Lhaca\Lhaca.exe" Pdf2iPad
+"C:\Program Files\à≥èkÅEìWäJ\Lhaca\Lhaca.exe" JPPBook
 sleep 15
 
-move /Y Pdf2iPad\Pdf2iPad.zip ..\
+move /Y JPPBook\JPPBook.zip ..\
 cd ..
 pause
