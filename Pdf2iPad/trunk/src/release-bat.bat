99163e06fa26b6460afc8f214d4f053d93199c6e
Katsuhiko Okano <okano@synergysoftware.jp>
2011-05-20 12:57:16 +0900

    arrange with release-bat for Pdf2iPad.

--- Pdf2iPad/trunk/src/release-bat.bat
+++ Pdf2iPad/trunk/src/release-bat.bat
@@ -3,8 +3,13 @@ rmdir /S /Q release-wrk
 mkdir release-wrk
 mkdir release-wrk\JPPBook
 xcopy /S /E /I /Q JPPBook release-wrk\JPPBook
-mkdir release-wrk\JPPBook\SKBEngine
-xcopy /S /E /I /Q ..\..\..\SKBEngine\trunk\src\SKBEngine\SKBEngine\SKB*.? release-wrk\JPPBook\SKBEngine
+rmdir /S /Q release-wrk\JPPBook\SKBEngine\SKBEngine.xcodeproj
+rm    release-wrk\JPPBook\SKBEngine\README
+rmdir /S /Q release-wrk\JPPBook\SKBEngine\SKBEngine\en.lproj
+rm    release-wrk\JPPBook\SKBEngine\SKBEngine\main.m
+rm    release-wrk\JPPBook\SKBEngine\SKBEngine\SKBEngine-Info.plist
+rm    release-wrk\JPPBook\SKBEngine\SKBEngine\SKBEngine-Prefix.pch
+
 
 cd release-wrk
 "C:\Program Files\à≥èkÅEìWäJ\Lhaca\Lhaca.exe" JPPBook
