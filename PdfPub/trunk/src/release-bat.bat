1a8d745023a7e110fb767f1110e52345b282c99d
Katsuhiko Okano <okano@synergysoftware.jp>
2011-05-20 12:56:39 +0900

    arrange with release-bat for PdfPub.

--- PdfPub/trunk/src/release-bat.bat
+++ PdfPub/trunk/src/release-bat.bat
@@ -3,8 +3,12 @@ rmdir /S /Q release-wrk
 mkdir release-wrk
 mkdir release-wrk\SakuttoBook
 xcopy /S /E /I /Q SakuttoBook release-wrk\SakuttoBook
-mkdir release-wrk\SakuttoBook\SKBEngine
-xcopy /S /E /I /Q ..\..\..\SKBEngine\trunk\src\SKBEngine\SKBEngine\SKB*.? release-wrk\SakuttoBook\SKBEngine
+rmdir /S /Q release-wrk\SakuttoBook\SKBEngine\SKBEngine.xcodeproj
+rm    release-wrk\SakuttoBook\SKBEngine\README
+rmdir /S /Q release-wrk\SakuttoBook\SKBEngine\SKBEngine\en.lproj
+rm    release-wrk\SakuttoBook\SKBEngine\SKBEngine\main.m
+rm    release-wrk\SakuttoBook\SKBEngine\SKBEngine\SKBEngine-Info.plist
+rm    release-wrk\SakuttoBook\SKBEngine\SKBEngine\SKBEngine-Prefix.pch
 
 cd release-wrk
 "C:\Program Files\à≥èkÅEìWäJ\Lhaca\Lhaca.exe" SakuttoBook
