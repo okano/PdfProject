a9e83116890cd2970c0ea0b0d386e4bc74d8784b
okano <okano@synergysoftware.jp>
2011-05-31 14:34:33 +0900

    add file ContentPlayerView : UIViewController.

---
+++ PdfPub/trunk/src/SakuttoBook/Classes/ContentPlayerView.h
@@ -0,0 +1,16 @@
+//
+//  ContentPlayerView.h
+//  SakuttoBook
+//
+//  Created by okano on 11/05/31.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ContentPlayerView : UIViewController {
+    
+}
+
+@end
