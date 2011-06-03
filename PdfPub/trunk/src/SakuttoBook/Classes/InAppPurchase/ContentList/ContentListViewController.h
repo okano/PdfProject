f51fa84c6250e32912a4b78327d62f2d0cdfeaec
okano <okano@synergysoftware.jp>
2011-06-03 11:26:03 +0900

    (implementing:)add files for payment.

---
+++ PdfPub/trunk/src/SakuttoBook/Classes/InAppPurchase/ContentList/ContentListViewController.h
@@ -0,0 +1,24 @@
+//
+//  ContentListViewController.h
+//  PurchaseTest04
+//
+//  Created by okano on 11/05/24.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Define.h"
+
+//@class PurchaseTest04AppDelegate;	//
+#import "SakuttoBookAppDelegate.h"
+#import "InAppPurchaseDefine.h"
+@class SakuttoBookViewController;
+
+@interface ContentListViewController : UITableViewController {
+	SakuttoBookAppDelegate* appDelegate;
+}
+
+- (void)showImagePlayer:(ContentId)cid;
+- (void)showPaymentConductorView:(ContentId)cid;
+
+@end
