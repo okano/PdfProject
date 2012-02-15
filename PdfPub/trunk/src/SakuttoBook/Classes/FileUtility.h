488608dcbefddb4e15c7aa6bdf714098cd8f03e6
Katsuhiko Okano <okano@synergysoftware.jp>
2012-02-15 11:57:50 +0900

    add method from from JPPBook.(master branch, 49587f2da09f5e2a54935a023b8fac253ad8d619, 2011-01-11)

--- PdfPub/trunk/src/SakuttoBook/Classes/FileUtility.h
+++ PdfPub/trunk/src/SakuttoBook/Classes/FileUtility.h
@@ -8,12 +8,19 @@
 
 #import <Foundation/Foundation.h>
 #import "Utility.h"
+#import "ContentFileUtility.h"
 #import "Define.h"
 #import "InAppPurchaseDefine.h"
 
 @interface FileUtility : NSObject {
     
 }
+//Pdf(Original) file.
++ (NSString*)getPdfFilename;
++ (NSString*)getPdfFilename:(ContentId)cId;
+#define TEST_PDF_FILENAME	@"document.pdf"
+
+//Each page cache.
 + (NSString*)getPageFilenameFull:(int)pageNum;
 + (NSString*)getPageFilenameFull:(int)pageNum WithContentId:(ContentId)cid;
 + (NSString*)getThumbnailFilenameFull:(int)pageNum;
@@ -21,14 +28,36 @@
 
 //CSV file parser.
 + (NSArray*)parseDefineCsv:(NSString*)filename;
++ (NSArray*)parseDefineCsvWithFullFilename:(NSString*)filenameFull;
 + (NSArray*)parseDefineCsv:(NSString*)filename contentId:(ContentId)cid;
++ (NSArray*)parseDefineCsvFromString:(NSString*)text;
+//CSV filename utility.
++ (NSString*)getCsvFilenameInFolder:(NSString*)filename contentId:(ContentId)cid;
++ (NSString*)getCsvFilenameInMainBundle:(NSString*)filename contentId:(ContentId)cid;
 
 #pragma mark - like POSIX file uty.
++ (NSArray*)fileList:(NSString*)path;
 + (BOOL)existsFile:(NSString*)fileNameFull;
 + (void)makeDir:(NSString*)fileNameFull;
 + (void)removeFile:(NSString*)fileNameFull;
++ (BOOL)res2file:(NSString*)res fileNameFull:(NSString*)filenameFull;	//Resource to File.
 
+
+//String cleaner
++ (NSString*)cleanString:(NSString*)str;
 @end
 
-
+//
+#define CSVFILE_PDFDEFINE		@"pdfDefine"
+//
+#define CSVFILE_URLLINK			@"urlLinkDefine"
+//
+#define CSVFILE_MOVIE			@"movieDefine"
 #define CSVFILE_SOUND			@"soundDefine"
+#define CSVFILE_PAGEJUMPLINK 	@"pageJumpLinkDefine"
+#define CSVFILE_INPAGE_SCROLLVIEW	@"inPageScrollViewDefine"
+#define CSVFILE_INPAGE_PDF		@"inPagePdfDefine"
+#define CSVFILE_INPAGE_PNG		@"inPagePngDefine"
+#define CSVFILE_POPOVER_IMAGE	@"popoverImageDefine"
+//
+#define CSVFILE_TOC				@"tocDefine"
