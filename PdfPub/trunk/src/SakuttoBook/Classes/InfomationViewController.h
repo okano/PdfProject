56194dc2edb1652d8911d2ebbb28f9005eeed4f7
Katsuhiko Okano <okano@synergysoftware.jp>
2012-03-23 11:00:09 +0900

    fix miss function returns type.

--- PdfPub/trunk/src/SakuttoBook/Classes/InfomationViewController.h
+++ PdfPub/trunk/src/SakuttoBook/Classes/InfomationViewController.h
@@ -30,7 +30,7 @@
 @property (nonatomic, retain) UIButton* bookSupportPageButton;
 @property (nonatomic, retain) UILabel* licenceNumberLabel;
 
-- (void)closeThisView:(id)sender;
+- (IBAction)closeThisView:(id)sender;
 - (void)setBookInfoFromDefineFile;
 - (void)showWebView:(NSString*)urlString;
 - (IBAction)openBookSupportPage:(id)sender;
