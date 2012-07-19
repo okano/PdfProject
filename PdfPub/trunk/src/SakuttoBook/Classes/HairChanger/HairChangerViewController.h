3a987ce2bd48d707ab5369d89813a6c6fcf9a890
Katsuhiko Okano <okano@synergysoftware.jp>
2012-07-19 20:10:07 +0900

    connect IBOutlet in HairChangerViewController.

--- PdfPub/trunk/src/SakuttoBook/Classes/HairChanger/HairChangerViewController.h
+++ PdfPub/trunk/src/SakuttoBook/Classes/HairChanger/HairChangerViewController.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 #import "SakuttoBookAppDelegate.h"
+#define MAX_POSE  9
+#define MAX_SCENE 8
 
 @interface HairChangerViewController : UIViewController <UIScrollViewDelegate> {
+	//Scene view.
+	IBOutlet UIImageView* sceneImageView;
+	//Person view.
+	IBOutlet UIImageView* personImageView;
+	IBOutlet UIView* personContainerView;
+	//Button
+	IBOutlet UIView* buttonContainerView;
+	
 	//Image Picker.
 	UIPopoverController* popoverController;
 	bool isShownImagePicker;
