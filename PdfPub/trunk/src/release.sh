09c48385e83cde92830799326dc0148b3d5aa6c7
Katsuhiko Okano <okano@synergysoftware.jp>
2012-09-19 15:25:24 +0900

    fix folder for ReleaseHash.h in release.sh

--- PdfPub/trunk/src/release.sh
+++ PdfPub/trunk/src/release.sh
@@ -5,7 +5,7 @@ mkdir $RELEASE_DIR
 cp -r SakuttoBook $RELEASE_DIR/
 
 ver=`git log -1 --pretty='format:#define RELEASE_HASH   @"%h,%ci"'`
-echo ${ver} > ./SakuttoBook/Classes/ReleaseHash.h
+echo ${ver} > $RELEASE_DIR/SakuttoBook/Classes/ReleaseHash.h
 
 cd $RELEASE_DIR
 rm -rf ./SakuttoBook/DerivedData
