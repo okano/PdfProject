9f4b7d2b9f434828d64f8cd4a938cefc072d5c50
Katsuhiko Okano <okano@synergysoftware.jp>
2012-10-20 15:36:08 +0900

    add mail send function.

--- PdfPub/trunk/docs/example-project/mailSendTest01/mailSendTest01/ViewController.m
+++ PdfPub/trunk/docs/example-project/mailSendTest01/mailSendTest01/ViewController.m
@@ -14,16 +14,58 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
+//@see: http://www.mikamiz.jp/dev/iphone/a0005.html
+//
+- (IBAction)pushMailButton:(id)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	NSLog(@"mail button pushed");
+	if( [MFMailComposeViewController canSendMail ] == NO ) {
+		NSLog(@"mail cannot send.");
+		
+		[ [ [ [ UIAlertView alloc ] initWithTitle:@"エラー" message:@"メールアカウントを設定してください" delegate:nil cancelButtonTitle:nil otherButtonTitles:nil ] autorelease ] show ];
+		return;
+	}
+	
+
+	//
+	MFMailComposeViewController *controller = [[[MFMailComposeViewController alloc] init] autorelease];
+	
+	[ controller setSubject:@"テストメール" ];
+	[ controller setToRecipients:[ NSArray arrayWithObject:@"main@example.com" ] ];
+	[ controller setCcRecipients:[ NSArray arrayWithObjects:@"cc1@example.com", @"cc2@excample.com", nil ] ];
+	[ controller setBccRecipients:[ NSArray arrayWithObjects:@"bcc1@example.com", @"bcc2@excample.com", nil ] ];
+	[ controller setMessageBody:@"サンプルメールn２行目n３行目" isHTML:NO ];
+	controller.mailComposeDelegate = self;
+	
+	[ self presentModalViewController:controller animated:YES ];
+
 }
 
-- (void)didReceiveMemoryWarning
+
+//<MFMailComposeViewControllerDelegate>
+
+- (void)mailComposeController:(MFMailComposeViewController*)controller
+		  didFinishWithResult:(MFMailComposeResult)result
+						error:(NSError*)error
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	
+	if( error != nil )
+	{
+		NSLog( @"エラーが発生しました。" );
+		[controller dismissModalViewControllerAnimated:YES];
+		return;
+	}
+	
+	switch( result )
+	{
+		case MFMailComposeResultSent: NSLog( @"メールを送信" ); break;
+		case MFMailComposeResultSaved: NSLog( @"メールを保存" ); break;
+		case MFMailComposeResultCancelled: NSLog( @"キャンセル" ); break;
+		case MFMailComposeResultFailed: NSLog( @"失敗" ); break;
+		default: NSLog( @"不明な結果" ); break;
+	}
+	[controller dismissModalViewControllerAnimated:YES];
+	
 }
 
 @end
