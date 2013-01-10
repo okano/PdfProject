a27f35a4d2748379aa3e2a775d542e1b6e221cc0
Katsuhiko Okano <okano@synergysoftware.jp>
2013-01-10 21:33:47 +0900

    add toolbar into ContentListImageViewController.

--- PdfPub/trunk/src/SakuttoBook/Classes/InAppPurchase/ContentList/ContentListImageViewController.h
+++ PdfPub/trunk/src/SakuttoBook/Classes/InAppPurchase/ContentList/ContentListImageViewController.h
@@ -7,8 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SakuttoBookAppDelegate.h"	//for read "Define.h" (it cannot load directory...)
 //#import "ContentListViewController.h"
 
 @interface ContentListImageViewController : UIViewController /* :ContentListViewController */
-
+{
+	IBOutlet UIToolbar* toolbar;
+	IBOutlet UIBarButtonItem *serverContentButton;
+	IBOutlet UIBarButtonItem *paymentHistoryButton;
+}
 @end
