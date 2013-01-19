c7c5c15b3bbf8f98e4615cdd5010e4249ee0733f
Katsuhiko Okano <okano@synergysoftware.jp>
2013-01-19 20:37:12 +0900

    arrange server information for MCS demo server.

--- PdfPub/trunk/src/SakuttoBook/Classes/UrlDefine.h
+++ PdfPub/trunk/src/SakuttoBook/Classes/UrlDefine.h
@@ -9,10 +9,11 @@
 
 //Server.
 //#define URL_BASE_OPDS_DEFAULT1		@"http://www2058uf.sakura.ne.jp:8080"
-#define URL_BASE_OPDS_DEFAULT1		@"http://www5066ua.sakura.ne.jp"		/* Demo Server. */
+#define URL_BASE_OPDS_DEFAULT1		@"http://www3200ui.sakura.ne.jp"		/* Demo Server. */
 
 //#define CREDENTIAL_REALM				@"Authorization Required"
-#define CREDENTIAL_REALM				@"Application"
+//#define CREDENTIAL_REALM				@"Application"
+#define CREDENTIAL_REALM				@"Authentication"
 
 
 //
